449e235046125d927f8a40db5971225b373b2547
meinvictor <vi.mein37@gmail.com>
2023-05-04 11:13:27 +0300

    changed

--- my-app/src/components/portfolio/Portfolio.jsx
+++ my-app/src/components/portfolio/Portfolio.jsx
@@ -2,22 +2,23 @@ import styles from "./Portfolio.module.scss";
 import items from "./data/portfolioData.js";
 
 const Portfolio = () => {
+  
   return (
     <div className={styles.pContainer}>
       {items.map((item, index) => (
-          <div className={styles.pItem} key={index}>
-            <div className={styles.pItemImg}>
-              <img src={item.pImg} alt={"itemImg"} />
+        <div className={styles.pItem} key={index}>
+          <div className={styles.pItemImg}>
+            <img src={item.pImg} alt={"itemImg"} />
+          </div>
+          <div className={styles.pItemText}>
+            <div className={styles.pItemName}>
+              <h3>{item.pName}</h3>
             </div>
-            <div className={styles.pItemText}>
-              <div className={styles.pItemName}>
-                <h3>{item.pName}</h3>
-              </div>
-              <div className={styles.pItemDesribe}>
-                <h5>{item.pDesc}</h5>
-              </div>
+            <div className={styles.pItemDesribe}>
+              <h5>{item.pDesc}</h5>
             </div>
           </div>
+        </div>
       ))}
     </div>
   );
