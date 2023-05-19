86f64d2f2c8648052f0b400429c7f71b9b4d810e
meinvictor <vi.mein37@gmail.com>
2023-05-19 18:21:45 +0300

    about me page

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -1,10 +1,56 @@
 import styles from "./About.module.scss";
+import Progress from "./progress/Progress";
 
 function About() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={styles.aboutContainer}>
+        <div className={styles.skills}>
+          <Progress/>
+        </div>
+        <div className={styles.about}>
+          <div className={styles.container}>
+            <div className={styles.box}>
+              <span className={styles.title}>WEB DEVELOPMENT</span>
+              <div>
+                <strong>
+                  Good knowledge and experience in developing web
+                  applications using React. I have an understanding of basic
+                  React concepts such as components, state, events, and
+                  component lifecycle.
+                </strong>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.container} ${styles.containerAdditional}`}>
+            <div className={styles.box}>
+              <span className={styles.title}>APP DESIGN</span>
+              <div>
+                <strong>
+                  I am well versed in HTML, CSS and JavaScript required for web
+                  application development. I have skills in styling React
+                  components using CSS(SASS).
+                </strong>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.container} ${styles.containerAdditional2}`}>
+            <div className={styles.box}>
+              <span className={styles.title}>
+                MOTIVATION
+              </span>
+              <div>
+                <strong>
+                  Looking for an opportunity to work on web projects where I can
+                  develop my React development skills and learn from experienced
+                  developers.
+                </strong>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ my-app/src/components/about/progress/Progress.jsx
@@ -0,0 +1,57 @@
+import styles from "./Progress.css";
+
+const Progress = () => {
+  return (
+    <>
+      <div className="progress">
+        <div className="dashboard">
+          <svg>
+            <circle className="bg" cx="57" cy="57" r="52" />
+            <circle className="meter-1" cx="57" cy="57" r="52" />
+            <text x="50%" y="55%" textAnchor="middle" className="text">
+              HTML/CSS
+            </text>
+          </svg>
+
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-2" cx="57" cy="57" r="52" />
+            <text x="50%" y="55%" textAnchor="middle" className="text">
+              JAVASCRIPT
+            </text>
+          </svg>
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-3" cx="57" cy="57" r="52" />
+            <text x="50%" y="55%" textAnchor="middle" className="text">
+              REACT
+            </text>
+          </svg>
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-4" cx="57" cy="57" r="52" />
+            <text x="50%" y="55%" textAnchor="middle" className="text">
+              REDUX
+            </text>
+          </svg>
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-5" cx="57" cy="57" r="52" />
+            <text x="50%" y="55%" textAnchor="middle" className="text">
+              GIT
+            </text>
+          </svg>
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-6" cx="57" cy="57" r="52" />
+            <text x="50%" y="55%" textAnchor="middle" className="text">
+              FIGMA
+            </text>
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Progress;
